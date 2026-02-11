0f1d3cb970abdc5fa7150e682018e7017145c109
brajeshkumarDeveloper <brajeshkewat8349@gmail.com>
2026-02-11 20:19:16 +0530

    fixed the private key

--- terraform/main.tf
+++ terraform/main.tf
@@ -99,7 +99,8 @@ resource "aws_instance" "brajesh" {
   connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    private_key = var.private_key
+    # private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
     host        = self.public_ip
   }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "public_key" {
   description = "Public SSH key"
   type        = string
 }
+
+variable "private_key" {
+  type      = string
+  sensitive = true
+}
